9bd7456f583274541d9b5a0784bde094e1b5c103
Nagababu890 <Nagababu.Chirra@ibm.com>
2025-09-05 11:10:12 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,16 @@
+package ACTIVITY;
+
+public class Activity1 {
+	public static void main(String[] args) {
+        Car benz = new Car();
+        benz.make = 2014;
+        benz.color = "Black";
+        benz.transmission = "Manual";
+
+        
+        benz.displayCharacteristics();
+        benz.accelarate();
+        benz.brake();
+}
+
+}
